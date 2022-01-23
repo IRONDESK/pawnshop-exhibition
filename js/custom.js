c632e1d4efa23a221df4ca28b49d3980d93d7588
SOOCHUL SON <87234410+IRONDESK@users.noreply.github.com>
2022-01-23 23:59:45 +0900

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,53 @@
+﻿
+$(function(){
+
+//변수 ht에 브라우저 높이 값을 저장
+    var ht= $(window).height();
+    //브라우저의 높이값을 section의 높이 값으로 지정
+    $("section").height(ht);
+    //브라우저가 리사이즈될 때마다 브라우저와 section의 높이 값을 갱신
+    $(window).on("resize", function(){
+        var ht=$(window).height();
+        $("section").height(ht);
+    });
+    //마우스의 움직임에 반응하는 서브 이미지 설정하기
+    $("section").on("mousemove", function(e){
+        //변수 posX에 마우스 커서의 x축 위치 저장
+        var posX=e.pageX;
+        //변수 posY에 마우스 커서의 y축 위치 저장
+        var posY=e.pageY;
+
+        //첫번째 박스의 이미지 위치 값을 마우스 커서의 위치 값과 연동
+        $(".p11").css({"right":20-(posX/30), "bottom":20-(posY/30)});
+        $(".p12").css({"right":130+(posX/20), "bottom":40-(posY/30)});
+        $(".p13").css({"right":60+(posX/20), "top":180+(posY/20)});
+        
+        //두번째 박스의 이미지 위치 값을 마우스 커서의 위치 값과 연동
+        $(".p21").css({"right":-180-(posX/30), "bottom":-480-(posY/30)});
+        $(".p22").css({"right":130+(posX/50), "bottom":40-(posY/50)});
+        
+         //세번째 박스의 이미지 위치 값을 마우스 커서의 위치 값과 연동
+         $(".p31").css({"right":280-(posX/30), "bottom":30-(posY/30)});
+         $(".p32").css({"right":110+(posX/20), "bottom":-270+(posY/20)});
+         $(".p33").css({"right":-70+(posX/20), "bottom":-130+(posY/20)});
+
+          //네번째 박스의 이미지 위치 값을 마우스 커서의 위치 값과 연동
+        $(".p41").css({"right":20-(posX/30), "bottom":20-(posY/30)});
+        $(".p42").css({"right":130+(posX/20), "bottom":40-(posY/30)});
+    });
+    
+    //주메뉴 클릭시, 자동으로 상하 스크롤시키기
+    $("#menu li").on("click", function(e){
+        e.preventDefault(); //a요소가 클릭시 링크가 동작해 깜빡이는 것을 막아줌.
+        //변수 ht에 브라우저의 높이 값 저장
+        var ht=$(window).height();
+        
+        //변수 i에 현재 클릭한 li의 순서값을 지정
+        var i=$(this).index();
+
+        //브라우저의 높이 값*박스의 순서값은 현재 박스의 스크롤 위치 값과 동일
+        var nowTop = i*ht;
+
+        //해당 스크롤의 위치 값으로 문서를 이동
+        $("html,body").stop().animate({"scrollTop":nowTop}, 1400);
+    });})
---
+++ js/modal.js
@@ -0,0 +1,161 @@
+////// 공중전화팀 소개
+    var modal3 = document.querySelector(".modal3"); 
+    var trigger3 = document.querySelector(".trigger3"); //버튼class명
+    var closeButton3 = document.querySelector(".close-button3"); 
+    var cancelButton3 = document.querySelector("#cancel3");
+
+   //console.log(modal);
+
+   function toggleModal3() {  //팝업클래스
+        modal3.classList.toggle("show-modal3"); //팝업클래스
+    }
+
+   function windowOnClick3(event) { 
+        if (event.target === modal3) { 
+            toggleModal3(); 
+        } 
+    }
+
+    trigger3.addEventListener("click", toggleModal3); //버튼클래스를 클릭할 때 = 팝업클래스출력
+    closeButton3.addEventListener("click", toggleModal3); 
+    cancel3.addEventListener("click", toggleModal3); 
+    window.addEventListener("click", windowOnClick3); 
+
+////// 교통티켓팀 소개
+    var modal4 = document.querySelector(".modal4"); 
+    var trigger4 = document.querySelector(".trigger4"); //버튼class명
+    var closeButton4 = document.querySelector(".close-button4"); 
+    var cancelButton4 = document.querySelector("#cancel4");
+
+   //console.log(modal);
+
+   function toggleModal4() {  //팝업클래스
+        modal4.classList.toggle("show-modal4"); //팝업클래스
+    }
+
+   function windowOnClick4(event) { 
+        if (event.target === modal4) { 
+            toggleModal4(); 
+        } 
+    }
+
+    trigger4.addEventListener("click", toggleModal4); //버튼클래스를 클릭할 때 = 팝업클래스출력
+    closeButton4.addEventListener("click", toggleModal4); 
+    cancel4.addEventListener("click", toggleModal4); 
+    window.addEventListener("click", windowOnClick4); 
+
+
+
+    ////// 사진앨범 소개
+    var modal5 = document.querySelector(".modal5"); 
+    var trigger5 = document.querySelector(".trigger5"); //버튼class명
+    var closeButton5 = document.querySelector(".close-button5"); 
+    var cancelButton5 = document.querySelector("#cancel5");
+
+   //console.log(modal);
+
+   function toggleModal5() {  //팝업클래스
+        modal5.classList.toggle("show-modal5"); //팝업클래스
+    }
+
+   function windowOnClick5(event) { 
+        if (event.target === modal5) { 
+            toggleModal5(); 
+        } 
+    }
+
+    trigger5.addEventListener("click", toggleModal5); //버튼클래스를 클릭할 때 = 팝업클래스출력
+    closeButton5.addEventListener("click", toggleModal5); 
+    cancel5.addEventListener("click", toggleModal5); 
+    window.addEventListener("click", windowOnClick5); 
+
+    ////// 열쇠 소개
+    var modal6 = document.querySelector(".modal6"); 
+    var trigger6 = document.querySelector(".trigger6"); //버튼class명
+    var closeButton6 = document.querySelector(".close-button6"); 
+    var cancelButton6 = document.querySelector("#cancel6");
+
+   //console.log(modal);
+
+   function toggleModal6() {  //팝업클래스
+        modal6.classList.toggle("show-modal6"); //팝업클래스
+    }
+
+   function windowOnClick6(event) { 
+        if (event.target === modal6) { 
+            toggleModal6(); 
+        } 
+    }
+
+    trigger6.addEventListener("click", toggleModal6); //버튼클래스를 클릭할 때 = 팝업클래스출력
+    closeButton6.addEventListener("click", toggleModal6); 
+    cancel6.addEventListener("click", toggleModal6); 
+    window.addEventListener("click", windowOnClick6); 
+
+    ////// 우체통 소개
+
+    var modal7 = document.querySelector(".modal7"); 
+    var trigger7 = document.querySelector(".trigger7"); //버튼class명
+    var closeButton7 = document.querySelector(".close-button7"); 
+    var cancelButton7 = document.querySelector("#cancel7");
+
+   //console.log(modal);
+
+   function toggleModal7() {  //팝업클래스
+        modal7.classList.toggle("show-modal7"); //팝업클래스
+    }
+
+   function windowOnClick7(event) { 
+        if (event.target === modal7) { 
+            toggleModal7(); 
+        } 
+    }
+
+    trigger7.addEventListener("click", toggleModal7); //버튼클래스를 클릭할 때 = 팝업클래스출력
+    closeButton7.addEventListener("click", toggleModal7); 
+    cancel7.addEventListener("click", toggleModal7); 
+    window.addEventListener("click", windowOnClick7); 
+    ////// 종이지도 소개
+    var modal8 = document.querySelector(".modal8"); 
+    var trigger8 = document.querySelector(".trigger8"); //버튼class명
+    var closeButton8 = document.querySelector(".close-button8"); 
+    var cancelButton8 = document.querySelector("#cancel8");
+
+   //console.log(modal);
+
+   function toggleModal8() {  //팝업클래스
+        modal8.classList.toggle("show-modal8"); //팝업클래스
+    }
+
+   function windowOnClick8(event) { 
+        if (event.target === modal8) { 
+            toggleModal8(); 
+        } 
+    }
+
+    trigger8.addEventListener("click", toggleModal8); //버튼클래스를 클릭할 때 = 팝업클래스출력
+    closeButton8.addEventListener("click", toggleModal8); 
+    cancel8.addEventListener("click", toggleModal8); 
+    window.addEventListener("click", windowOnClick8); 
+    ////// 카세트테이프 소개
+    var modal9 = document.querySelector(".modal9"); 
+    var trigger9 = document.querySelector(".trigger9"); //버튼class명
+    var closeButton9 = document.querySelector(".close-button9"); 
+    var cancelButton9 = document.querySelector("#cancel9");
+
+   //console.log(modal);
+
+   function toggleModal9() {  //팝업클래스
+        modal9.classList.toggle("show-modal9"); //팝업클래스
+    }
+
+   function windowOnClick9(event) { 
+        if (event.target === modal9) { 
+            toggleModal9(); 
+        } 
+    }
+
+    trigger9.addEventListener("click", toggleModal9); //버튼클래스를 클릭할 때 = 팝업클래스출력
+    closeButton9.addEventListener("click", toggleModal9); 
+    cancel9.addEventListener("click", toggleModal9); 
+    window.addEventListener("click", windowOnClick9); 
